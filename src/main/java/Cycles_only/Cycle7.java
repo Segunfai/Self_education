b3b2114984687ce2c3c43e64d80620a184448798
Segunfai <segunfai111@yandex.ru>
2025-12-03 22:32:30 +0500

    Последовательность Фибоначчи

--- src/main/java/Cycles_only/Cycle7.java
+++ src/main/java/Cycles_only/Cycle7.java
@@ -12,11 +12,43 @@ public class Cycle7 {
         int a = 0;
         int b = 1;
 
-        for (int i = 0; i <= num; ++i) {
+        for (int i = 0; i < num; i++) {
+            System.out.print(a + " ");
             int s = a + b;
             a = b;
             b = s;
-            System.out.print(s + " ");
         }
+        /*
+        Альтернатива от нейронки
+        {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите N: ");
+        int num = input.nextInt();
+
+        if (num <= 0) {
+            System.out.println("Введите число больше 0!");
+            return;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        // Выводим первые два числа
+        System.out.print(a + " ");
+        if (num > 1) {
+            System.out.print(b + " ");
+        }
+
+        // Выводим остальные (N-2) чисел
+        for (int i = 2; i < num; i++) {
+            int next = a + b;
+            System.out.print(next + " ");
+            a = b;
+            b = next;
+        }
+
+        input.close();
+         */
     }
 }
