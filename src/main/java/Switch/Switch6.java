5f5e903582769d947501aac35b987dd9cd0e0da1
Segunfai <segunfai111@yandex.ru>
2026-02-28 18:00:49 +0500

    Опитимизация Switch6

--- src/main/java/Switch/Switch6.java
+++ src/main/java/Switch/Switch6.java
@@ -32,22 +32,25 @@ public class Switch6 {
 
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-        String mal = "Малые: ";
-        String mid = "Средние: ";
-        String big = "Большие: ";
+        StringBuilder mal = new StringBuilder("Малые: ");
+        StringBuilder mid = new StringBuilder("Средние: ");
+        StringBuilder big = new StringBuilder("Большие: ");
 
         for (int choose : nums) {
             switch (choose) {
-                case 1, 2, 3, 4, 5 -> mal += choose + " ";
-                case 6, 7, 8, 9, 10 -> mid += choose + " ";
-                case 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 -> big += choose + " ";
+                case 1, 2, 3, 4, 5 -> mal.append(choose).append(" ");
+                case 6, 7, 8, 9, 10 -> mid.append(choose).append(" ");
+                case 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 -> big.append(choose).append(" ");
                 default -> System.out.print("Вне диапазона ");
             }
         }
 
-        System.out.print(mal + "\n");
-        System.out.print(mid + "\n");
-        System.out.print(big + "\n");
+        String res1 = mal.toString().trim();
+        String res2 = mid.toString().trim();
+        String res3 = big.toString().trim();
+        System.out.print(res1 + "\n");
+        System.out.print(res2 + "\n");
+        System.out.print(res3 + "\n");
 
         System.out.println();
         input.close();
