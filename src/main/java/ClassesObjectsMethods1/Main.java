f8d6a1c9233573afa156a72089de3f5338bd36c2
Segunfai <segunfai111@yandex.ru>
2026-03-01 18:03:54 +0500

    Выполнение Com1

---
+++ src/main/java/ClassesObjectsMethods1/Main.java
@@ -0,0 +1,34 @@
+/*
+Задание 1: Класс "Студент"
+
+Создайте класс 'Student' с полями:
+
+- `name` (имя)
+
+- `age` (возраст)
+
+- `grade` (средний балл)
+
+
+Добавьте метод `displayInfo()`, который выводит информацию о студенте. В main создайте объект и вызовите метод.
+
+**Пример:**
+
+text
+
+Имя: Анна
+Возраст: 20
+Средний балл: 4.8
+
+ */
+
+package ClassesObjectsMethods1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student ann = new Student("Анна", 28, 4.8);
+        ann.displayInfo();
+
+    }
+}
---
+++ src/main/java/ClassesObjectsMethods1/Student.java
@@ -0,0 +1,20 @@
+package ClassesObjectsMethods1;
+
+class Student {
+
+    String name;
+    int age;
+    double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    void displayInfo(){
+        System.out.print("Имя: " + name + "\n");
+        System.out.print("Возраст: " + age + "\n");
+        System.out.println("Средний балл: " + grade + "\n");
+    }
+}
