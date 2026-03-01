276bc7a4a811dd680ba52f9a5a47fdbeb276735a
Segunfai <segunfai111@yandex.ru>
2026-03-01 19:01:36 +0500

    Выполнение задания 2 в классе Student

--- src/main/java/ClassesObjectsMethods1/task1/Student.java
+++ src/main/java/ClassesObjectsMethods1/task1/Student.java
@@ -1,14 +1,56 @@
+/*
+**Задание 2: Геттеры и сеттеры**
+
+Добавьте в класс `Student`:
+
+- Приватные поля
+
+- Конструктор с параметрами
+
+- Геттеры и сеттеры для всех полей
+
+- В сеттере для возраста добавьте проверку (возраст > 0)
+ */
+
 package ClassesObjectsMethods1.task1;
 
 class Student {
 
-    String name;
-    int age;
-    double grade;
+    private String name;
+    private int age;
+    private double grade;
 
     public Student(String name, int age, double grade) {
         this.name = name;
         this.age = age;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.print("Задан некорректный возраст!");
+        }
+    }
+
+    public void setGrade(double grade) {
         this.grade = grade;
     }
 
