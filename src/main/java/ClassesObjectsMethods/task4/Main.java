944eea6c88dd63f3219404f77faf951535f7edae
Segunfai <segunfai111@yandex.ru>
2026-03-14 22:01:02 +0500

    Выполнение задания 5 Массив объектов

---
+++ src/main/java/ClassesObjectsMethods/task4/Main.java
@@ -0,0 +1,8 @@
+package ClassesObjectsMethods.task4;
+
+public class Main {
+
+    Student Greg = new Student("Greg", 23, 4.3);
+    Student Ann = new Student("Ann", 19, 4.8);
+    Student Peter = new Student("Peter", 20, 3.9);
+}
---
+++ src/main/java/ClassesObjectsMethods/task4/Student.java
@@ -0,0 +1,63 @@
+/*
+### **Задание 5: Массив объектов**
+
+Создайте массив из 3 объектов `Student`. Заполните данными (можно с клавиатуры). Выведите информацию о всех студентах, у которых средний балл выше 4.0.
+
+ */
+
+package ClassesObjectsMethods.task4;
+
+import java.util.Scanner;
+
+public class Student {
+
+    Student[] student = new Student[3];
+
+    Scanner input = new Scanner(System.in);
+
+    private String name;
+    private int age;
+    private double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.print("Задан некорректный возраст!");
+        }
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    void displayInfo(){
+        System.out.print("Имя: " + name + "\n");
+        System.out.print("Возраст: " + age + "\n");
+        System.out.println("Средний балл: " + grade + "\n");
+    }
+
+
+}
