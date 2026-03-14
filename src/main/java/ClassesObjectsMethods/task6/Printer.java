e841b3014dad7e7970f0b7001f1a79b7166b7468
Segunfai <segunfai111@yandex.ru>
2026-03-14 23:51:54 +0500

    Фикс по заданию 6

--- src/main/java/ClassesObjectsMethods/task6/Printer.java
+++ src/main/java/ClassesObjectsMethods/task6/Printer.java
@@ -4,28 +4,33 @@ import java.util.Arrays;
 
 class Program{
     public static void main(String[] args) {
-        Printer print = new Printer();
+        Printer prnt = new Printer();
+        System.out.println(prnt.print("ok"));
+        System.out.println(prnt.print("Good", 3));
+        System.out.println(prnt.print(5));
+        System.out.println(Arrays.toString(prnt.print(new int[]{1, 2, 3, 5})));
     }
 }
 
 public class Printer {
 
-    void print(String text) {
-        System.out.println(text);
+    String print(String text) {
+        return text;
     }
 
-    void print (String text, int times) {
-        for (int i = 0; i == times; i++) {
+    String print (String text, int times) {
+        for (int i = 1; i < times; i++) {
             System.out.println(text);
         }
+        return text;
     }
 
-    void print (int number) {
-        System.out.println(number);
+    int print (int number) {
+        return number;
     }
 
-    void print (int[] array) {
-        System.out.println(Arrays.toString(array));
+    int[] print (int[] array) {
+        return array;
     }
 
 }
