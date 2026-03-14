52d982c0e747a69312f7eb85452099ce4e65b47c
Segunfai <segunfai111@yandex.ru>
2026-03-15 00:07:28 +0500

    Исправления и добивание задания 6

--- src/main/java/ClassesObjectsMethods/task6/Printer.java
+++ src/main/java/ClassesObjectsMethods/task6/Printer.java
@@ -1,3 +1,15 @@
+/*
+Создайте класс `Printer` с перегруженными методами `print()`:
+
+- `print(String text)` - выводит строку
+
+- `print(String text, int times)` - выводит строку указанное количество раз
+
+- `print(int number)` - выводит число
+
+- `print(int[] array)` - выводит массив чисел
+ */
+
 package ClassesObjectsMethods.task6;
 
 import java.util.Arrays;
@@ -5,8 +17,8 @@ import java.util.Arrays;
 class Program{
     public static void main(String[] args) {
         Printer prnt = new Printer();
-        System.out.println(prnt.print("ok"));
-        System.out.println(prnt.print("Good", 3));
+        prnt.print("ok");
+        prnt.print("Good" , 3);
         System.out.println(prnt.print(5));
         System.out.println(Arrays.toString(prnt.print(new int[]{1, 2, 3, 5})));
     }
@@ -14,15 +26,14 @@ class Program{
 
 public class Printer {
 
-    String print(String text) {
-        return text;
+    void print(String text) {
+        System.out.println(text);
     }
 
-    String print (String text, int times) {
-        for (int i = 1; i < times; i++) {
+    void print (String text, int times) {
+        for (int i = 0; i < times; i++) {
             System.out.println(text);
         }
-        return text;
     }
 
     int print (int number) {
