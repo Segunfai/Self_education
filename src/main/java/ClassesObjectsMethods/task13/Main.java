28aae588796af6433d6859c548b5cea2548faa65
Segunfai <segunfai111@yandex.ru>
2026-06-14 18:00:05 +0500

    Работа с отрицательными числами задание 13

--- src/main/java/ClassesObjectsMethods/task13/Main.java
+++ src/main/java/ClassesObjectsMethods/task13/Main.java
@@ -8,18 +8,59 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Введите текущие часы: ");
-        int hour = input.nextInt();
-        System.out.print("Введите текушие минуты: ");
-        int min = input.nextInt();
-        System.out.print("Введите текущие секунды: ");
-        int sec = input.nextInt();
-        System.out.print("Введите добавленные часы: ");
-        int hours = input.nextInt();
-        System.out.print("Введите добавленные минуты: ");
-        int minutes = input.nextInt();
-        System.out.print("Введите добавленные секунды: ");
-        int seconds = input.nextInt();
+        int hour;
+        do {
+            System.out.print("Введите текущие часы: ");
+            hour = input.nextInt();
+            if (hour < 0) {
+                System.out.println("Вы ввели отрицательное число!");
+            }
+        } while (hour < 0);
+
+        int min;
+        do {
+            System.out.print("Введите текушие минуты: ");
+            min = input.nextInt();
+            if (min < 0) {
+                System.out.println("Вы ввели отрицательное число!");
+            }
+        } while (min < 0);
+
+        int sec;
+        do {
+            System.out.print("Введите текущие секунды: ");
+            sec = input.nextInt();
+            if (sec < 0) {
+                System.out.println("Вы ввели отрицательное число!");
+            }
+        } while (sec < 0);
+
+        int hours;
+        do {
+            System.out.print("Введите добавленные часы: ");
+            hours = input.nextInt();
+            if (hours < 0) {
+                System.out.println("Вы ввели отрицательное число!");
+            }
+        } while (hours < 0);
+
+        int minutes;
+        do {
+            System.out.print("Введите добавленные минуты: ");
+            minutes = input.nextInt();
+            if (minutes < 0) {
+                System.out.println("Вы ввели отрицательное число!");
+            }
+        } while (minutes < 0);
+
+        int seconds;
+        do {
+            System.out.print("Введите добавленные секунды: ");
+            seconds = input.nextInt();
+            if (seconds < 0) {
+                System.out.println("Вы ввели отрицательное число!");
+            }
+        } while (seconds < 0);
 
         Timer time = new Timer(hour, min, sec);
 
